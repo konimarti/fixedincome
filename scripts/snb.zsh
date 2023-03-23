c7daf8b84c03983ad7a4bad6d4eed232befd5d06
Koni Marti <koni.marti@gmail.com>
2023-03-23 16:48:11 +0100

    scripts: update snb script to produce correct term structure json

--- scripts/snb.zsh
+++ scripts/snb.zsh
@@ -1,2 +1,2 @@
 #!/bin/zsh
- curl -X GET https://data.snb.ch/api/cube/rendopar/data/csv/en | tail -6 | awk -F'"' '{print $4"="$6}' | jo | jq '' 
+ curl -X GET https://data.snb.ch/api/cube/rendopar/data/csv/en | tail -6 | awk -F'"' '{print $4"="$6}' | jo | jq '. += {"spread": 0.0}'
